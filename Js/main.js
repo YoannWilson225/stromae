f892535ea9bc827e8ed2adbaa357f0346b7c1f0b
Yoann-Philippe Wilson <79014805+YoannWilson225@users.noreply.github.com>
2022-04-13 16:44:11 +0000

    premiere page

---
+++ Js/main.js
@@ -0,0 +1,72 @@
+/**
+* Template Name: Personal - v4.6.0
+* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+    "use strict";
+  
+    /**
+     * Mobile nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function(e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+    })
+  
+    /**
+     * Scrool with ofset on links with a class name .scrollto
+     */
+    on('click', '#navbar .nav-link', function(e) {
+      let section = select(this.hash)
+      if (section) {
+        e.preventDefault()
+  
+        let navbar = select('#navbar')
+        let header = select('#header')
+        let sections = select('section', true)
+        let navlinks = select('#navbar .nav-link', true)
+  
+        navlinks.forEach((item) => {
+          item.classList.remove('active')
+        })
+  
+        this.classList.add('active')
+  
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile')
+          let navbarToggle = select('.mobile-nav-toggle')
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
+        }
+  
+        if (this.hash == '#header') {
+          header.classList.remove('header-top')
+          sections.forEach((item) => {
+            item.classList.remove('section-show')
+          })
+          return;
+        }
+  
+        if (!header.classList.contains('header-top')) {
+          header.classList.add('header-top')
+          setTimeout(function() {
+            sections.forEach((item) => {
+              item.classList.remove('section-show')
+            })
+            section.classList.add('section-show')
+  
+          }, 350);
+        } else {
+          sections.forEach((item) => {
+            item.classList.remove('section-show')
+          })
+          section.classList.add('section-show')
+        }
+  
+        scrollto(this.hash)
+      }
+    }, true)
+  })()
