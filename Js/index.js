b9b981041067279436871f3f33dbc4eb94881c3a
Yoann-Philippe Wilson <79014805+YoannWilson225@users.noreply.github.com>
2022-04-15 00:11:22 +0000

    fin du travail

---
+++ Js/index.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4wl4zX0zki_WgMl5-f-ebgke9KsYeyAM",
+  authDomain: "stromae-a722a.firebaseapp.com",
+  databaseURL: "https://stromae-a722a-default-rtdb.firebaseio.com",
+  projectId: "stromae-a722a",
+  storageBucket: "stromae-a722a.appspot.com",
+  messagingSenderId: "853197105659",
+  appId: "1:853197105659:web:e4029496bf0b8dee02035e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
